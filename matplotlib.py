9355fbda82b5938448c3b51cc3008206b4fa4255
PrakashMakwanaa <147391259+PrakashMakwanaa@users.noreply.github.com>
2023-10-09 15:55:00 +0530

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,22 @@
+# Matplotlib
+
+#matplotlib graphs
+
+# 1. Linear Plot
+# 2. Scatter Plot
+# 3. Bar Plot
+# 4. Stem Plot
+# 5. Step Plot
+# 6. Hist Plot
+# 7. Box Plot
+# 8. Pie Plot
+# 9. Fill_Between Plot
+
+import matplotlib.pyplot as plt 
+
+x=[2,5,7,4]
+y=[3,6,1,8]
+
+plt.plot(x,y)
+plt.show()
+
